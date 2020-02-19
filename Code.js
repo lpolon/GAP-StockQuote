b4c5534d63662535791dd04c62164f0c66f75842
lpolon <lpolon@gmail.com>
2020-02-19 14:18:27 -0300

    given a docId, find symbols to substitute

--- Code.js
+++ Code.js
@@ -1,2 +1,24 @@
-function myFunction() {
+function readGDoc() {
+  const { docId } = options;
+  const body = DocumentApp.openById(docId).getBody();
+  const paragraphsArr = body.getParagraphs();
+  const foundSymbols = {};
+
+  paragraphsArr.forEach((e) => {
+    const parText = e.getText();
+    parText.split(' ').forEach((word) => {
+      if (word.includes('&') && word.length > 1) {
+        foundSymbols[word.substring(1)] = null;
+      }
+    });
+  });
+  Logger.log(foundSymbols);
 }
+
+/*
+getText
+split in words
+find words that matches &
+forEach match, get formatted string
+iterate over object, calling replace on body
+*/
