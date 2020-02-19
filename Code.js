5ba771413e48cad440564c0031990bc38e4b8fbe
lpolon <lpolon@gmail.com>
2020-02-19 15:17:44 -0300

    add util functions to manipulate array of arrays

--- Code.js
+++ Code.js
@@ -3,7 +3,6 @@ function getSymbolsFromDoc() {
   const body = DocumentApp.openById(docId).getBody();
   const paragraphsArr = body.getParagraphs();
   const foundSymbols = {};
-
   paragraphsArr.forEach((e) => {
     const parText = e.getText();
     parText.split(' ').forEach((word) => {
@@ -15,21 +14,8 @@ function getSymbolsFromDoc() {
   return foundSymbols;
 }
 
-function lookupSymbolInfo() {
-  const {
-    sheetInfo: { ssId, sheetName },
-  } = options;
-}
-
-
-function formatStockQuoteString(ticker, trigger) {
-  const stringArr = trigger.split(' ');
-  const firstElement = Number(stringArr[0]).toFixed(2);
-  let secondElement = stringArr[1].slice(1, -1);
-  if (secondElement[0] === '-') {
-    secondElement = secondElement.replace('-', '▼');
-  } else {
-    secondElement = String(`▲${secondElement}`);
-  }
-  return `[${firstElement} ${secondElement}]`;
+function lookupSymbolInfo(key) {
+  const {ssId, sheetName} = options.sheetInfo
+  const tableValues = getTableValues(ssId, sheetName, 1, 1);
+  Logger.log(tableValues);
 }
